6b14f8f3b19c60a663f95057a7ec93f1c7538561
J. Daniels <ayn.pray@gmail.com>
2024-01-13 11:05:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# XMLjs
+![Build Status](https://badgen.net/bundlephobia/min/@ayn/xmljs) ![Build Status](https://badgen.net/bundlephobia/minzip/@ayn/xmljs)
+
+XMLjs is a tiny beautiful XML markup generator.
+
+## API
+```js
+const XML = require('@ayn/xmljs')
+
+let doc = new XMLjs()
+    .fruits(el=>{
+        el.mango({ color: 'green'}, 'This mango is not ripe')
+        el.bananas(el=>{
+            el.babybanana('Quite small')
+            el.plantain('Delicious')
+        })
+    })
+    
+console.log(doc.getMarkup())
+```
+Simple and elegant. Constructor takes a string as its argument that allows you to set the documents prolog.
+
+Child elements return an object holding information on siblings and its parent, they also have access to the `getMarkup()` method which returns a subset of the markup.
+
+## Output
+```xml
+<fruits>
+    <mango color="green">This mango is not ripe</mango>
+    <bananas>
+        <babybanana>Quite small</babybanana>
+        <plantain>Delicious</plantain>
+    </bananas>
+</fruits>
+```
---
+++ index.js
@@ -0,0 +1,129 @@
+class XMLjs {
+    constructor(prolog = '') {
+
+        // Docs info tag
+        this.config = { prolog }
+
+        // Document structure
+        this.dom={}
+        
+        // Allow wild card functions
+        return new Proxy(this, this)
+    }
+
+    get(target, prop) {
+
+        // If doesn't exist
+        if(!this[prop]) {
+
+            // Run our function
+            return function(...Args) {
+
+                // defaults
+                let attributes = {},
+                    content = '',
+                    cb = ()=>{},
+                    target = (Args[3]) ? Args[3] : this.dom
+                 
+                // Adjust first 3 args by type
+                for(let [i, arg] of Args.entries()) { 
+                    if(i == 3 ) break                   
+                    switch(typeof arg) {
+                        case 'object':
+                          attributes = arg
+                          break;
+                        case 'string':
+                          content = arg
+                          break;
+                        case 'function':
+                          cb = arg
+                          break;
+                      } 
+                }
+                        
+                // Branch
+                let obj = {
+                    [prop]: {
+                        attributes,
+                        content,
+                        children: [],
+                    }
+                }
+
+                // Append to parent
+                if(target.push) target.push(obj)
+                    else target[prop] = obj[prop]
+        
+                // Modify cb to append to right object
+                cb(new Proxy({}, {
+                    get:(target, sub_prop)=>{
+                        // Prop seems to be symbol when loggin
+                        if(typeof sub_prop != 'symbol') {
+                            if(sub_prop=='getMarkup') return ()=>this.getMarkup(obj)
+                            else if(sub_prop=='parent') return prop
+                            else if(sub_prop=='siblings') return obj[prop].children
+                            else return (attributes, sub_content, cb)=> this[sub_prop](attributes, sub_content, cb, obj[prop].children) 
+                        }
+                        
+                        else return () => { return { parent: prop, siblings: obj[prop].children } } 
+                    }
+                }))
+    
+                return this
+    
+            }
+        }
+    
+        else {
+            return this[prop]
+        }
+    }
+
+    getMarkup(obj) {
+        
+        if(!obj) { 
+            obj = this.dom
+            var markup = this.config.prolog
+        } else var markup = ''
+
+        let parse = (obj, content ='') => {
+
+            let markup = ''
+
+            for(let el in obj) {
+
+                let formatted_attributes = ''
+                
+                // Handle attributes
+                if(obj[el].attributes)
+                    for(let attribute in obj[el].attributes) 
+                        formatted_attributes += `${attribute}='${obj[el].attributes[attribute]}'`
+                
+                // Handle content
+                if(typeof obj[el].content == 'string')   
+                    content = obj[el].content
+
+                let inner_markup = ''
+
+                // Handle children
+                if(obj[el].children) 
+                    obj[el].children.forEach(child=>{
+                        inner_markup += parse(child, content)
+                    })
+                
+                // Generate tag
+                markup += `<${el}${(formatted_attributes) ? ' '+formatted_attributes:""}>${ content+inner_markup+'</'+el+'>'}`
+
+                return markup
+            }
+
+        }
+
+        markup += parse(obj || this.dom)
+      
+        return markup
+        
+    }
+}
+
+module.exports = XMLjs
